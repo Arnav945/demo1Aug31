18bfb3110f0108df1bdd713939e9bad218ed0adb
Arnav945 <63356444+Arnav945@users.noreply.github.com>
2022-09-06 14:15:07 -0500

    Test Submit

---
+++ src/main/java/com/example/demo1aug31/repositories/OrderRepository.java
@@ -0,0 +1,25 @@
+package com.example.demo1aug31.repositories;
+
+import com.example.demo1aug31.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Example;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    @Override
+    List<Order> findAll();
+
+    @Override
+    Order save(Order order);
+
+    @Override
+    void deleteById(Long id);
+
+    @Override
+    Optional<Order> findById(Long id);
+
+
+}
+
+
